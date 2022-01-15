0a79d8d13ffe0033413d9b512fe01c9f45ae77cb
Alexander Spiridonov <alexrspiridonov@gmail.com>
2022-01-15 10:33:08 +0300

    finish hello word example render

---
+++ src/example.js
@@ -0,0 +1,20 @@
+//const element = <h1 title="foo">Hello</h1>
+//changed to React.creteElement by transpiler
+//const element = React.createElement("h1", { title: "foo" }, "Hello");
+//result of React.createElement is simple object of certain form
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "hello",
+  },
+};
+const container = document.getElementById("root");
+//ReactDom.render(element, container);
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+node.appendChild(text);
+container.appendChild(node);
+//serve this as script to empty html page with container node
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const element = <h1 title="foo">Hello</h1>;
-const container = document.getElementById("root");
-ReactDom.render(element, container);
