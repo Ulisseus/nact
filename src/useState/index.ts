0f5e65275cd20512f9ff1a1471f150acd8ecf9aa
Alexander Spiridonov <alexrspiridonov@gmail.com>
2022-02-20 11:20:31 +0300

    add generics to useState hook

--- src/useState/index.ts
+++ src/useState/index.ts
@@ -2,18 +2,19 @@ import { wipFiber, hookIndex } from '../state';
 import { assignStateVariable, currentRoot } from '../state';
 import { Fiber, STATE_VARIABLES } from '../types';
 
-export const useState = (initial: any) => {
+export const useState = <T>(initial: T) => {
     const oldHook = wipFiber?.alternate?.hooks[hookIndex];
     const hook = { state: oldHook ? oldHook.state : initial, queue: [] };
+
     wipFiber.hooks.push(hook);
     assignStateVariable(hookIndex + 1, STATE_VARIABLES.hookIndex);
 
     const actions = oldHook ? oldHook.queue : [];
 
-    actions.forEach((action) => {
+    actions.forEach((action: (args: T) => void) => {
         hook.state = action(hook.state);
     });
-    const setState = (action: Function) => {
+    const setState = (action: (args: T) => void) => {
         hook.queue.push(action);
         const wipRoot: Fiber = {
             dom: currentRoot.dom,
