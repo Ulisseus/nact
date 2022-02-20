3363fd3e3e40b0d1b25dcce1911790bb467f6e60
Alexander Spiridonov <alexrspiridonov@gmail.com>
2022-02-20 10:56:24 +0300

    update useState hook

--- src/useState/index.ts
+++ src/useState/index.ts
@@ -1,11 +1,33 @@
 import { wipFiber, hookIndex } from '../state';
-import { assignStateVariable } from '../state';
-import { STATE_VARIABLES } from '../types';
+import { assignStateVariable, currentRoot } from '../state';
+import { Fiber, STATE_VARIABLES } from '../types';
 
 export const useState = (initial: any) => {
     const oldHook = wipFiber?.alternate?.hooks[hookIndex];
-    const hook = { state: oldHook ? oldHook.state : initial };
+    const hook = { state: oldHook ? oldHook.state : initial, queue: [] };
     wipFiber.hooks.push(hook);
     assignStateVariable(hookIndex + 1, STATE_VARIABLES.hookIndex);
-    return [hook.state];
+
+    const actions = oldHook ? oldHook.queue : [];
+
+    actions.forEach((action) => {
+        hook.state = action(hook.state);
+    });
+    const setState = (action: Function) => {
+        hook.queue.push(action);
+        const wipRoot: Fiber = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot,
+            child: null,
+            parent: null,
+            sibling: null,
+            hooks: null,
+            type: currentRoot.type,
+        };
+        assignStateVariable(wipRoot, STATE_VARIABLES.nextUnitOfWork);
+        assignStateVariable([], STATE_VARIABLES.deletions);
+    };
+
+    return [hook.state, setState];
 };
